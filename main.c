7ec3bac815640705f0aead1acc5d0e5b8fe8f069
Ismail Degani <idegani@mit.edu>
2019-06-21 16:43:57 -0400

    working C++/OpenVG framegrabber (GPU-accelerated)

--- main.c
+++ main.c
@@ -36,125 +36,142 @@ struct buffer {
   size_t length;
 };
 
-typedef struct tagEsContext
+typedef struct
 {
-    GLint width;
-    GLint height;
-
     EGLDisplay display;
     EGLSurface surface;
     EGLContext context;
-} EsContext;
-EsContext state;
+    EGLConfig config;
+} EGL_STATE_T;
 
-static void get_native_display_size(uint32_t* width, uint32_t* height)
+struct egl_manager {
+    EGLNativeDisplayType xdpy;
+    EGLNativeWindowType xwin;
+    EGLNativePixmapType xpix;
+
+    EGLDisplay dpy;
+    EGLConfig conf;
+    EGLContext ctx;
+
+    EGLSurface win;
+    EGLSurface pix;
+    EGLSurface pbuf;
+    EGLImageKHR image;
+
+    EGLBoolean verbose;
+    EGLint major, minor;
+};
+
+EGL_STATE_T state, *p_state = &state;
+
+
+void init_egl(EGL_STATE_T *state)
 {
-    // get display size
-    if (graphics_get_display_size(0 /*LCD*/,
-                                  width,
-                                  height) < 0) {
-        perror("Failed to graphics_get_display_size");
-        exit(EXIT_FAILURE);
-    }
-}
+    EGLint num_configs;
+    EGLBoolean result;
 
-static NativeWindowType
-create_native_window(uint32_t width, uint32_t height)
-{
-    VC_RECT_T dst_rect;
-    vc_dispmanx_rect_set(&dst_rect, 0, 0, width, height);
+    //bcm_host_init();
 
-    VC_RECT_T src_rect;
-    vc_dispmanx_rect_set(&src_rect, 0, 0, (width<<16), (height<<16));
+    static const EGLint attribute_list[] =
+	{
+	    EGL_RED_SIZE, 5,
+	    EGL_GREEN_SIZE, 6,
+	    EGL_BLUE_SIZE, 5,
+	    EGL_ALPHA_SIZE, 0,
+	    EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+	    EGL_RENDERABLE_TYPE, EGL_OPENVG_BIT,
+	    EGL_NONE
+	};
 
-    DISPMANX_DISPLAY_HANDLE_T dispman_display = vc_dispmanx_display_open(0 /*LCD*/);
-    DISPMANX_UPDATE_HANDLE_T dispman_update = vc_dispmanx_update_start(0);
-    DISPMANX_ELEMENT_HANDLE_T dispman_element =
-        vc_dispmanx_element_add(dispman_update,
-                                dispman_display,
-                                0/*layer*/,
-                                &dst_rect,
-                                0/*src*/,
-                                &src_rect,
-                                DISPMANX_PROTECTION_NONE,
-                                0/*alpha*/,
-                                0/*clamp*/,
-                                DISPMANX_NO_ROTATE);
-    static EGL_DISPMANX_WINDOW_T nativewindow;
-    nativewindow.element = dispman_element;
-    nativewindow.width = width;
-    nativewindow.height = height;
-    vc_dispmanx_update_submit_sync(dispman_update);
-    return (NativeWindowType)&nativewindow;
-}
+    static const EGLint context_attributes[] =
+	{
+	    EGL_CONTEXT_CLIENT_VERSION, 2,
+	    EGL_NONE
+	};
 
-void init_ogl(EsContext *state)
-{
     // get an EGL display connection
-    state->display = eglGetDisplay(state->display);
+    state->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 
     // initialize the EGL display connection
-    if (eglInitialize(state->display, NULL, NULL) == EGL_FALSE) {
-        perror("Failed to eglInitialize");
-        exit(EXIT_FAILURE);
-    }
+    result = eglInitialize(state->display, NULL, NULL);
 
     // get an appropriate EGL frame buffer configuration
-    static const EGLint attribute_list[] = {
-        EGL_RED_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_BLUE_SIZE, 8,
-        EGL_BUFFER_SIZE, 8,
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_NONE
-    };
-    EGLint num_config = 0;
-    EGLConfig config;
-    if (eglChooseConfig(state->display, attribute_list,
-                        &config, 1, &num_config) == EGL_FALSE) {
-        perror("Failed to eglChooseConfig");
-        exit(EXIT_FAILURE);
-    }
+    result = eglChooseConfig(state->display, attribute_list, &state->config, 1, &num_configs);
+    assert(EGL_FALSE != result);
 
-    // bind to OpenGLES API
-    if (eglBindAPI(EGL_OPENGL_ES_API) == EGL_FALSE) {
-        perror("Failed to eglBindAPI");
-        exit(EXIT_FAILURE);
-    }
+     result = eglBindAPI(EGL_OPENVG_API);
+    assert(EGL_FALSE != result);
 
     // create an EGL rendering context
-    static const EGLint context_attributes[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 2,
-        EGL_NONE
-    };
-    state->context = eglCreateContext(state->display, config,
-                                      EGL_NO_CONTEXT, context_attributes);
-    if (state->context == EGL_NO_CONTEXT) {
-        perror("Failed to eglCreateContext");
-        exit(EXIT_FAILURE);
-    }
-
-    // create an EGL Window surface
-    get_native_display_size(&state->width, &state->height);
-    NativeWindowType nativewindow =
-        create_native_window(state->width, state->height);
-    state->surface = eglCreateWindowSurface(state->display, config,
-                                            nativewindow, NULL);
-    if (state->surface == EGL_NO_SURFACE) {
-        perror("Failed to eglCreateWindowSurface");
-        exit(EXIT_FAILURE);
-    }
-
-    // connect the context to the surface
-    if (eglMakeCurrent(state->display, state->surface,
-                       state->surface, state->context) == EGL_FALSE) {
-        perror("Failed to eglMakeCurrent");
-        exit(EXIT_FAILURE);
-    }
+    state->context = eglCreateContext(state->display,
+				      state->config, EGL_NO_CONTEXT,
+				      NULL);
+				      // breaks if we use this: context_attributes);
+    assert(state->context!=EGL_NO_CONTEXT);
 }
 
+void init_dispmanx(EGL_DISPMANX_WINDOW_T *nativewindow) {
+    int32_t success = 0;
+    uint32_t screen_width;
+    uint32_t screen_height;
 
-void deinit(EsContext *state)
+    DISPMANX_ELEMENT_HANDLE_T dispman_element;
+    DISPMANX_DISPLAY_HANDLE_T dispman_display;
+    DISPMANX_UPDATE_HANDLE_T dispman_update;
+    VC_RECT_T dst_rect;
+    VC_RECT_T src_rect;
+
+    bcm_host_init();
+
+    // create an EGL window surface
+    success = graphics_get_display_size(0 /* LCD */,
+					&screen_width,
+					&screen_height);
+    assert( success >= 0 );
+
+    dst_rect.x = 0;
+    dst_rect.y = 0;
+    dst_rect.width = screen_width;
+    dst_rect.height = screen_height;
+
+    src_rect.x = 0;
+    src_rect.y = 0;
+    src_rect.width = screen_width << 16;
+    src_rect.height = screen_height << 16;
+
+    dispman_display = vc_dispmanx_display_open( 0 /* LCD */);
+    dispman_update = vc_dispmanx_update_start( 0 );
+
+    dispman_element =
+	vc_dispmanx_element_add(dispman_update, dispman_display,
+				0/*layer*/, &dst_rect, 0/*src*/,
+				&src_rect, DISPMANX_PROTECTION_NONE,
+				0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
+
+    // Build an EGL_DISPMANX_WINDOW_T from the Dispmanx window
+    nativewindow->element = dispman_element;
+    nativewindow->width = screen_width;
+    nativewindow->height = screen_height;
+    vc_dispmanx_update_submit_sync(dispman_update);
+
+    printf("Got a Dispmanx window\n");
+}
+
+void egl_from_dispmanx(EGL_STATE_T *state,
+		       EGL_DISPMANX_WINDOW_T *nativewindow) {
+    EGLBoolean result;
+
+    state->surface = eglCreateWindowSurface(state->display,
+					    state->config,
+					    nativewindow, NULL );
+    assert(state->surface != EGL_NO_SURFACE);
+
+    // connect the context to the surface
+    result = eglMakeCurrent(state->display, state->surface, state->surface, state->context);
+    assert(EGL_FALSE != result);
+}
+
+/*void deinit(EsContext *state)
 {
 	// free the egsImage vgDestroyPaint
 	eglMakeCurrent(state->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
@@ -162,7 +179,13 @@ void deinit(EsContext *state)
 	eglTerminate(state->display);
 	assert(eglGetError() == EGL_SUCCESS);
 	eglReleaseThread();
-}
+}*/
+//VGImage img = 0;
+
+/*void init_frame(buffer *buf){
+
+}*/
+
 
 
 static void xioctl(int fh, int request, void *arg)
@@ -198,10 +221,33 @@ int main(int argc, char **argv)
 
         memset (fakebuf,128,640*480);
 
-        bcm_host_init();
+
+        EGL_DISPMANX_WINDOW_T nativewindow;
+
+        init_egl(p_state);
+        init_dispmanx(&nativewindow);
+        egl_from_dispmanx(p_state, &nativewindow);
+
+        float c = 1.0;
+        float clearColor[4] = {c, c, c, c};  // white, no transparency
+        vgSetfv(VG_CLEAR_COLOR, 4, clearColor);
+
+        vgClear(0, 0, 640, 480);
+
+        vg_img = vgCreateImage(VG_sL_8,
+                    640, 480,
+                    VG_IMAGE_QUALITY_NONANTIALIASED);
+        if (vg_img == VG_INVALID_HANDLE) {
+        fprintf(stderr, "Can't create simple image\n");
+        fprintf(stderr, "Error code %x\n", vgGetError());
+        exit(2);
+        }
+
+
+        // bcm_host_init();
 
         // OpenVG needs a surface to draw on, and EGL provides that
-        init_ogl(&state);
+        //init_ogl(&state);
 
         //glClearColor(0.0, 0.0, 0.0, 1.0);
         //glClear(GL_COLOR_BUFFER_BIT);
@@ -310,15 +356,24 @@ int main(int argc, char **argv)
                 vgImageSubData(vg_img, buffers[buf.index].start, fmt.fmt.pix.width,
                     VG_sL_8, 0, 0, fmt.fmt.pix.width, fmt.fmt.pix.height);
                 // center the image at (80,0)
-                int w_offset = (state.width - fmt.fmt.pix.width) / 2;
+                //int w_offset = (state.width - fmt.fmt.pix.width) / 2;
                 // vgSetPixels(w_offset, 0, vg_img, 0, 0, fmt.fmt.pix.width, fmt.fmt.pix.height);
                 vgSetPixels(0, 0, vg_img, 0, 0, fmt.fmt.pix.width, fmt.fmt.pix.height);
-                vgDrawImage(vg_img);
+                eglSwapBuffers(p_state->display, p_state->surface);
+
+                //vgSeti(VG_MATRIX_MODE, VG_MATRIX_IMAGE_USER_TO_SURFACE);
+                //vgLoadIdentity();
+                //vgTranslate(200, 30);
+
+                //vgDrawImage(vg_img);
+
+
+                //vgDrawImage(vg_img);
                 // eglSwapBuffers(state.display, state.surface);
                 // do we need this?
                 //eglSwapBuffers(state.display, state.surface);
 
-
+                vgFlush();
                 /*sprintf(out_name, "out%03d.ppm", i);
                 fout = fopen(out_name, "w");
                 if (!fout) {
@@ -333,6 +388,7 @@ int main(int argc, char **argv)
                 xioctl(fd, VIDIOC_QBUF, &buf);
         }
 
+        sleep(3);
         type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         xioctl(fd, VIDIOC_STREAMOFF, &type);
         for (i = 0; i < n_buffers; ++i)
@@ -340,6 +396,7 @@ int main(int argc, char **argv)
         v4l2_close(fd);
 
         vgDestroyImage(vg_img);
-        deinit(&state);
+        //deinit(&state);
         return 0;
+
 }
